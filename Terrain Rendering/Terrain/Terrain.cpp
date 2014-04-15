196711b9050389d61fe963216e0b55416c817220
Jeffrey Smalley <cloudwomega@gmail.com>
2014-04-15 15:08:36 -0400

    fix

--- Terrain Rendering/Terrain/Terrain.cpp
+++ Terrain Rendering/Terrain/Terrain.cpp
@@ -195,11 +195,21 @@ void Terrain::LoadHeightmap()
 
 	std::ifstream inFile;
 	//RL Open the file.
+	inFile.open(mInfo.HeightMapFilename.c_str(), std::ios_base::binary); 
+	if(inFile) 
+	{ 
+		// Read the RAW bytes. 
+		inFile.read((char*)&in[0], (std::streamsize)in.size()); 
+		// Done with file. 
+		inFile.close(); 
+	} 
+	// Copy the array data into a float array and scale it. 
+	mHeightmap.resize(mInfo.HeightmapHeight * mInfo.HeightmapWidth, 0); 
+	for(UINT i = 0; i < mInfo.HeightmapHeight * mInfo.HeightmapWidth; ++i)
+	{ 
+		mHeightmap[i] = (in[i] / 255.0f)*mInfo.HeightScale; 
+	} 
 
-	//RL If the file contains data read the RAW bytes
-	//RL When complete close the file
-
-	//RL Copy the array data into a float array and scale it.
 }
 
 void Terrain::Smooth()
