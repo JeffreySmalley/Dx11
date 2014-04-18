12d8f6fbb2e4c24333c3aab987bcff26ec69ccaf
Jeffrey Smalley <cloudwomega@gmail.com>
2014-04-18 19:44:00 -0400

    done

--- Terrain Rendering/Terrain/TerrainDemo.cpp
+++ Terrain Rendering/Terrain/TerrainDemo.cpp
@@ -38,7 +38,17 @@ public:
 	void OnMouseMove(WPARAM btnState, int x, int y);
 
 private:
+	void DrawScene(const Camera& camera, bool water);
+	void BuildCubeFaceCamera(float x, float y, float z);
+	void BuildDynamicCubeMapViews();
 	void BuildWaveGeometryBuffers();
+	ID3D11DepthStencilView* mDynamicCubeMapDSV;
+	ID3D11RenderTargetView* mDynamicCubeMapRTV[6];
+	ID3D11ShaderResourceView* mDynamicCubeMapSRV;
+	D3D11_VIEWPORT mCubeMapViewport;
+	static const int CubeMapSize = 256;
+	Camera mCubeMapCamera[6];
+
 	ID3D11Buffer* mWavesVB;
 	ID3D11Buffer* mWavesIB;
 	ID3D11ShaderResourceView* mWavesMapSRV;
@@ -78,7 +88,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 
 TerrainApp::TerrainApp(HINSTANCE hInstance)
 : D3DApp(hInstance), mSky(0), mWalkCamMode(false), mWavesVB(0), mWavesIB(0), mWavesMapSRV(0),
-  mWaterTexOffset(0.0f, 0.0f)
+  mWaterTexOffset(0.0f, 0.0f), mDynamicCubeMapDSV(0), mDynamicCubeMapSRV(0)
 {
 	mMainWndCaption = L"Terrain Demo";
 	mEnable4xMsaa = false;
@@ -86,10 +96,18 @@ TerrainApp::TerrainApp(HINSTANCE hInstance)
 	mLastMousePos.x = 0;
 	mLastMousePos.y = 0;
 
+
+
+	mCam.SetPosition(0.0f, 10.0f, 100.0f);
+	BuildCubeFaceCamera(0.0f, 5.0f, 0.0f);
+
+	
+	for(int i = 0; i < 6; ++i)
+	{
+		mDynamicCubeMapRTV[i] = 0;
+	}
 	XMMATRIX I = XMMatrixIdentity();
 	XMStoreFloat4x4(&mWavesWorld, I);
-
-	mCam.SetPosition(0.0f, 2.0f, 100.0f);
 
 	mDirLights[0].Ambient  = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
 	mDirLights[0].Diffuse  = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -106,9 +124,10 @@ TerrainApp::TerrainApp(HINSTANCE hInstance)
 	mDirLights[2].Specular = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
 	mDirLights[2].Direction = XMFLOAT3(-0.57735f, -0.57735f, -0.57735f);
 	
-	mWavesMat.Ambient  = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
-	mWavesMat.Diffuse  = XMFLOAT4(1.0f, 1.0f, 1.0f, 0.5f);
+	mWavesMat.Ambient  = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+	mWavesMat.Diffuse  = XMFLOAT4(0.2f, 0.2f, 0.2f, 0.5f);
 	mWavesMat.Specular = XMFLOAT4(0.8f, 0.8f, 0.8f, 32.0f);
+	mWavesMat.Reflect  = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
 }
 
 TerrainApp::~TerrainApp()
@@ -120,6 +139,10 @@ TerrainApp::~TerrainApp()
 	ReleaseCOM(mWavesVB);
 	ReleaseCOM(mWavesIB);
 	ReleaseCOM(mWavesMapSRV);
+	ReleaseCOM(mDynamicCubeMapDSV);
+	ReleaseCOM(mDynamicCubeMapSRV);
+	for(int i = 0; i < 6; ++i)
+		ReleaseCOM(mDynamicCubeMapRTV[i]);
 
 	Effects::DestroyAll();
 	InputLayouts::DestroyAll();
@@ -153,6 +176,10 @@ bool TerrainApp::Init()
 	tii.HeightmapHeight = 2049;
 	tii.CellSpacing = 0.5f;
 
+	HR(D3DX11CreateShaderResourceViewFromFile(md3dDevice, 
+		L"Textures/water2.dds", 0, 0, &mWavesMapSRV, 0 ));
+
+	BuildDynamicCubeMapViews();
 	BuildWaveGeometryBuffers();
 	mTerrain.Init(md3dDevice, md3dImmediateContext, tii);
 	return true;
@@ -244,63 +271,121 @@ void TerrainApp::UpdateScene(float dt)
 
 void TerrainApp::DrawScene()
 {
+	ID3D11RenderTargetView* renderTargets[1];
+
+	// Generate the cube map.
+	//RL Set the viewport of the immediate context to the cube map viewport
+	md3dImmediateContext->RSSetViewports(1, &mCubeMapViewport);
+
+	
+	
+	//RL Bind the cube map as the render target
+	for(int i = 0; i < 6; ++i)
+	{
+		// Clear cube map face and depth buffer.
+		//RL Clear each RTV (HINT: reinterpret_cast<const float*>(&Colors::Silver) is important)
+		//RL Clear the Depth Stencil View (HINT: D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL is important)
+		md3dImmediateContext->ClearRenderTargetView(mDynamicCubeMapRTV[i], reinterpret_cast<const float*>(&Colors::Silver));
+		md3dImmediateContext->ClearDepthStencilView(mDynamicCubeMapDSV, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+		// Bind cube map face as render target.
+		renderTargets[0] = mDynamicCubeMapRTV[i];
+		md3dImmediateContext->OMSetRenderTargets(1, renderTargets, mDynamicCubeMapDSV);
+
+		// Draw the scene with the exception of the center sphere to this cube map face.
+		//RL Draw the scene using the overloaded function DrawScene (mCubeMapCamera is important here)
+		DrawScene(mCubeMapCamera[i], false);
+	}
+
+	// Restore old viewport and render targets.
+    md3dImmediateContext->RSSetViewports(1, &mScreenViewport);
+	renderTargets[0] = mRenderTargetView;
+    md3dImmediateContext->OMSetRenderTargets(1, renderTargets, mDepthStencilView);
+
+    // Have hardware generate lower mipmap levels of cube map.
+    md3dImmediateContext->GenerateMips(mDynamicCubeMapSRV);
+	
+	// Now draw the scene as normal, but with the center sphere.
 	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::Silver));
 	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+	
+	DrawScene(mCam, true);
+
+	HR(mSwapChain->Present(0, 0));
+}
+
+void TerrainApp::DrawScene(const Camera& camera, bool water)
+{
+	
 
 	md3dImmediateContext->IASetInputLayout(InputLayouts::Basic32);
     md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
  
 	float blendFactor[] = {0.0f, 0.0f, 0.0f, 0.0f};
 
+
+
 	UINT stride = sizeof(Vertex::Basic32);
     UINT offset = 0;
 
+	// Set per frame constants.
+	Effects::BasicFX->SetDirLights(mDirLights);
+	Effects::BasicFX->SetEyePosW(mCam.GetPosition());
+
 	ID3DX11EffectTechnique* landAndWavesTech;
+	landAndWavesTech = Effects::BasicFX->Light3ReflectTech;
+
+	D3DX11_TECHNIQUE_DESC techDesc;
+	md3dImmediateContext->IASetVertexBuffers(0, 1, &mWavesVB, &stride, &offset);
+	md3dImmediateContext->IASetIndexBuffer(mWavesIB, DXGI_FORMAT_R32_UINT, 0);
+	if(water)
+		{
+		landAndWavesTech->GetDesc( &techDesc );
+		for(UINT p = 0; p < techDesc.Passes; ++p)
+		{
+			//
+			// Draw the waves.
+			//
+			md3dImmediateContext->IASetVertexBuffers(0, 1, &mWavesVB, &stride, &offset);
+			md3dImmediateContext->IASetIndexBuffer(mWavesIB, DXGI_FORMAT_R32_UINT, 0);
+
+			// Set per object constants.
+			XMMATRIX world = XMLoadFloat4x4(&mWavesWorld);
+			XMMATRIX worldInvTranspose = MathHelper::InverseTranspose(world);
+			XMMATRIX worldViewProj = world*/*mCam*/camera.View()*/*mCam*/camera.Proj();
+		
+			Effects::BasicFX->SetWorld(world);
+			Effects::BasicFX->SetWorldInvTranspose(worldInvTranspose);
+			Effects::BasicFX->SetWorldViewProj(worldViewProj);
+			Effects::BasicFX->SetTexTransform(XMLoadFloat4x4(&mWaterTexTransform));
+			Effects::BasicFX->SetMaterial(mWavesMat);
+			Effects::BasicFX->SetDiffuseMap(mWavesMapSRV);
+			Effects::BasicFX->SetCubeMap(mDynamicCubeMapSRV);
+		
+			//RL Tell the context what blend state to use (HINT: OMSetBlendState)
+			md3dImmediateContext->OMSetBlendState(RenderStates::TransparentBS ,blendFactor, 0xffffffff);
+
+			landAndWavesTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+			md3dImmediateContext->DrawIndexed(3*mWaves.TriangleCount(), 0, 0);
+
+			// Restore default blend state
+			md3dImmediateContext->OMSetBlendState(0, blendFactor, 0xffffffff);
+		}
+	}
+
+
+	mTerrain.Draw(md3dImmediateContext, camera, mDirLights);
 	
-	landAndWavesTech = Effects::BasicFX->Light3Tech;
-
-
-	if( GetAsyncKeyState('1') & 0x8000 )
-		md3dImmediateContext->RSSetState(RenderStates::WireframeRS);
-
-	mTerrain.Draw(md3dImmediateContext, mCam, mDirLights);
-
-	md3dImmediateContext->RSSetState(0);
-
-	mSky->Draw(md3dImmediateContext, mCam);
 
 	// restore default states, as the SkyFX changes them in the effect file.
+	
+	mSky->Draw(md3dImmediateContext, camera);
+	//if( GetAsyncKeyState('1') & 0x8000 )
+		//md3dImmediateContext->RSSetState(RenderStates::WireframeRS);
+
+	
 	md3dImmediateContext->RSSetState(0);
 	md3dImmediateContext->OMSetDepthStencilState(0, 0);
-	D3DX11_TECHNIQUE_DESC techDesc;
-
-	landAndWavesTech->GetDesc( &techDesc );
-    for(UINT p = 0; p < techDesc.Passes; ++p)
-    {
-		md3dImmediateContext->IASetVertexBuffers(0, 1, &mWavesVB, &stride, &offset);
-		md3dImmediateContext->IASetIndexBuffer(mWavesIB, DXGI_FORMAT_R32_UINT, 0);
-
-		// Set per object constants.
-		XMMATRIX world = XMLoadFloat4x4(&mWavesWorld);
-		XMMATRIX worldInvTranspose = MathHelper::InverseTranspose(world);
-		XMMATRIX worldViewProj = mCam.ViewProj();
-		
-		Effects::BasicFX->SetWorld(world);
-		Effects::BasicFX->SetWorldInvTranspose(worldInvTranspose);
-		Effects::BasicFX->SetWorldViewProj(worldViewProj);
-		Effects::BasicFX->SetTexTransform(XMLoadFloat4x4(&mWaterTexTransform));
-		Effects::BasicFX->SetMaterial(mWavesMat);
-		Effects::BasicFX->SetDiffuseMap(mWavesMapSRV);
-
-		//RL Tell the context what blend state to use (HINT: OMSetBlendState)
-
-		landAndWavesTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
-		md3dImmediateContext->DrawIndexed(3*mWaves.TriangleCount(), 0, 0);
-
-		// Restore default blend state
-		md3dImmediateContext->OMSetBlendState(0, blendFactor, 0xffffffff);
-    }
-
 	HR(mSwapChain->Present(0, 0));
 }
 
@@ -380,4 +465,158 @@ void TerrainApp::BuildWaveGeometryBuffers()
     D3D11_SUBRESOURCE_DATA iinitData;
     iinitData.pSysMem = &indices[0];
     HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mWavesIB));
+}
+
+void TerrainApp::BuildCubeFaceCamera(float x, float y, float z)
+{
+	// Generate the cube map about the given position.
+	XMFLOAT3 center(x, y, z);
+	XMFLOAT3 worldUp(0.0f, 1.0f, 0.0f);
+
+	// Look along each coordinate axis.
+	XMFLOAT3 targets[6] = 
+	{
+		XMFLOAT3(x+1.0f, y, z), // +X
+		XMFLOAT3(x-1.0f, y, z), // -X
+		XMFLOAT3(x, y+1.0f, z), // +Y
+		XMFLOAT3(x, y-1.0f, z), // -Y
+		XMFLOAT3(x, y, z+1.0f), // +Z
+		XMFLOAT3(x, y, z-1.0f)  // -Z
+	};
+
+	// Use world up vector (0,1,0) for all directions except +Y/-Y.  In these cases, we
+	// are looking down +Y or -Y, so we need a different "up" vector.
+	XMFLOAT3 ups[6] = 
+	{
+		XMFLOAT3(0.0f, 1.0f, 0.0f),  // +X
+		XMFLOAT3(0.0f, 1.0f, 0.0f),  // -X
+		XMFLOAT3(0.0f, 0.0f, -1.0f), // +Y
+		XMFLOAT3(0.0f, 0.0f, +1.0f), // -Y
+		XMFLOAT3(0.0f, 1.0f, 0.0f),	 // +Z
+		XMFLOAT3(0.0f, 1.0f, 0.0f)	 // -Z
+	};
+
+	for(int i = 0; i < 6; ++i)
+	{
+		mCubeMapCamera[i].LookAt(center, targets[i], ups[i]);
+		mCubeMapCamera[i].SetLens(0.5f*XM_PI, 1.0f, 0.1f, 1000.0f);
+		mCubeMapCamera[i].UpdateViewMatrix();
+	}
+}
+
+void TerrainApp::BuildDynamicCubeMapViews()
+{
+	//
+	// Cubemap is a special texture array with 6 elements.
+	//
+    
+	//RL Describe the Texture Array
+	//RL Requires use of D3D11_TEXTURE2D_DESC
+	//RL HINT: CubeMapSize is a variable that is already defined
+	//RL HINT: D3D11_TEXTURE2D_DESC has a property ArraySize
+	//RL HINT: Format = DXGI_FORMAT_R8G8B8A8_UNORM
+	//RL HINT: BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE
+	//RL HINT: Don't forget the MiscFlags.
+	
+	D3D11_TEXTURE2D_DESC texDesc;
+	texDesc.Width = CubeMapSize;
+	texDesc.Height = CubeMapSize;
+	texDesc.MipLevels = 0;
+	texDesc.ArraySize =6;
+	texDesc.SampleDesc.Count =1;
+	texDesc.SampleDesc.Quality = 0;
+	texDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	texDesc.Usage=D3D11_USAGE_DEFAULT;
+	texDesc.BindFlags = D3D11_BIND_RENDER_TARGET|D3D11_BIND_SHADER_RESOURCE;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS |
+	D3D11_RESOURCE_MISC_TEXTURECUBE;
+	
+	//RL Create the Texture2D using cubeTex
+	ID3D11Texture2D* cubeTex = 0;
+	HR(md3dDevice->CreateTexture2D(&texDesc, 0, &cubeTex));
+	 //
+	 // Create a render target view to each cube map face 
+	 // (i.e., each element in the texture array).
+	 // 
+
+    D3D11_RENDER_TARGET_VIEW_DESC rtvDesc;
+    rtvDesc.Format = texDesc.Format;
+    rtvDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2DARRAY;
+    rtvDesc.Texture2DArray.ArraySize = 1;
+    rtvDesc.Texture2DArray.MipSlice = 0;
+
+    for(int i = 0; i < 6; ++i)
+    {
+        rtvDesc.Texture2DArray.FirstArraySlice = i;
+        HR(md3dDevice->CreateRenderTargetView(cubeTex, &rtvDesc, &mDynamicCubeMapRTV[i]));
+    }
+
+	//
+    // Create a shader resource view to the cube map.
+	//
+
+    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+    srvDesc.Format = texDesc.Format;
+    srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURECUBE;
+    srvDesc.TextureCube.MostDetailedMip = 0;
+	srvDesc.TextureCube.MipLevels = -1;
+
+    HR(md3dDevice->CreateShaderResourceView(cubeTex, &srvDesc, &mDynamicCubeMapSRV));
+
+	ReleaseCOM(cubeTex);
+
+	//
+	// We need a depth texture for rendering the scene into the cubemap
+	// that has the same resolution as the cubemap faces.  
+	//
+
+	//RL Create the depth texture
+	//RL Requires use of D3D11_TEXTURE2D_DESC
+	//RL HINT: CubeMapSize is important again
+	//RL HINT: BindFlags = D3D11_BIND_DEPTH_STENCIL
+	//RL HINT: Format = DXGI_FORMAT_D32_FLOAT;
+	//ID3D11DepthStencilView* mDynamicCubeMapDSV;
+
+	D3D11_TEXTURE2D_DESC depthTexDesc;
+    depthTexDesc.Width = CubeMapSize;
+    depthTexDesc.Height = CubeMapSize;
+    depthTexDesc.MipLevels = 1;
+    depthTexDesc.ArraySize = 1;
+    depthTexDesc.SampleDesc.Count = 1;
+    depthTexDesc.SampleDesc.Quality = 0;
+    depthTexDesc.Format = DXGI_FORMAT_D32_FLOAT;
+    depthTexDesc.Usage = D3D11_USAGE_DEFAULT;
+    depthTexDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    depthTexDesc.CPUAccessFlags = 0;
+    depthTexDesc.MiscFlags = 0;
+
+	//RL Create the depth texture using depthTex 
+
+	ID3D11Texture2D* depthTex = 0;
+	HR(md3dDevice->CreateTexture2D(&depthTexDesc, 0, &depthTex));
+
+    // Create the depth stencil view for the entire cube
+    D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc;
+    dsvDesc.Format = depthTexDesc.Format;
+	dsvDesc.Flags  = 0;
+    dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+    dsvDesc.Texture2D.MipSlice = 0;
+    HR(md3dDevice->CreateDepthStencilView(depthTex, &dsvDesc, &mDynamicCubeMapDSV));
+
+	ReleaseCOM(depthTex);
+
+	//
+	// Viewport for drawing into cubemap.
+	// 
+
+	//RL Set the values for the cube map viewport
+	//RL Again, CubeMapSize is important
+	//D3D11_VIEWPORT mCubeMapViewport;
+	mCubeMapViewport.TopLeftX = 0.0f;
+	mCubeMapViewport.TopLeftY = 0.0f;
+	mCubeMapViewport.Width = (float)CubeMapSize;
+	mCubeMapViewport.Height = (float)CubeMapSize;
+	mCubeMapViewport.MinDepth = 0.0f;
+	mCubeMapViewport.MaxDepth = 1.0f;
 }
